43037a034134bab1b1155711b85dba0b120aaf32
Harris Sidiropoulos <harris.sidiropoulos@gmail.com>
2012-12-05 19:17:50 +0200

    hbox

--- docs/examples/assets/js/directives/components.js
+++ docs/examples/assets/js/directives/components.js
@@ -23,21 +23,39 @@ angular.module('ngt', [])
                 if (typeof attrs.horizontalGap==="undefined") attrs.horizontalGap = "20px";
                 angular.forEach($(element).children(),function(child) {
                     if (typeof attrs.horizontalGap!=='undefined' && $(child).index()>0) {
-                        $(child).css('padding-left', attrs.horizontalGap);
+                        $(child).css('margin-left', attrs.horizontalGap);
+                        $(child).addClass('added-gap');
                     }
                 });
+
                 element.html('<div class="ngt-hbox">'+element.html()+'</div>');
 
                 return function linkFn(scope, element, attrs) {
+                    $(window).bind('resize', function() {
+                        setHorizontalGap();
+                    });
                     scope.$watch('horizontalGap', function(value) {
                         if (typeof value==="undefined") return;
                         scope.horizontalGap = value;
-                        angular.forEach($(element).children(),function(child) {
-                            if (typeof attrs.horizontalGap!=='undefined' && $(child).index()>0) {
-                                $(child).css('padding-left', value);
+                        setHorizontalGap(value);
+
+                    });
+                    function setHorizontalGap(value) {
+                        if (typeof value==="undefined") value = attrs.horizontalGap;
+                        if (value=="0px") return;
+                        angular.forEach($(element).find('.added-gap'),function(child) {
+                            $(child).css('margin-left', value);
+                            $(child).css('clear', 'none');
+                        });
+                        angular.forEach($(element).find('.added-gap'),function(child) {
+                            if ($(child).index()>0 && $(child).offset().top==$(child).prev().offset().top) {
+                                $(child).css('margin-left', value);
+                            } else {
+                                $(child).css('clear', 'left');
+                                $(child).css('margin-left', "0px");
                             }
                         });
-                    });
+                    }
                 }
             }
         }
