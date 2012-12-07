e5bbcce0fd7a46a0738ec0d453c5bf896664c204
Harris Sidiropoulos <harris.sidiropoulos@gmail.com>
2012-12-07 14:31:59 +0200

    components.js

--- docs/examples/assets/js/directives/components.js
+++ docs/examples/assets/js/directives/components.js
@@ -1,6 +1,44 @@
 'use strict';
 
 angular.module('ngt', [])
+    .directive('ngtAutoHeight', function($http, $parse, $compile) {
+        return {
+            restrict: 'A',
+
+            compile: function compileFn(element, attrs) {
+
+                $(window).bind('resize', function() {
+                    setHeight();
+                })
+
+                function setHeight() {
+                    var parent = element.parent()[0].tagName==="BODY"?$(window):$(element.parent()),
+                        parentHeight = parent.height(),
+                        elementExtraHeight = $(element).outerHeight(true)-$(element).height(),
+                        parentChildrenHeight = 0;
+
+                    $(element.parent()).children().each(function(index, child) {
+                        if ($(child).css('display')!=="none" && $(child).width()>0 && $(child).height()>0) {
+                            if ($(child).offset().left == $(element).offset().left) {
+                                if ($(element).css('position')==="absolute") {
+                                    if ($(child).css('position')==="absolute") {
+                                        parentChildrenHeight += $(child).outerHeight(true);
+                                    }
+                                } else {
+                                    parentChildrenHeight += $(child).outerHeight(true);
+                                }
+                            }
+                        }
+                    })
+                    parentChildrenHeight = parentChildrenHeight - $(element).outerHeight(true);
+                    parentChildrenHeight = parentChildrenHeight<0?0:parentChildrenHeight;
+                    $(element).height(parentHeight-elementExtraHeight-parentChildrenHeight);
+                }
+
+                setHeight();
+            }
+        }
+    })
     .directive('ngtTitle', function($http, $parse, $compile) {
         return {
             restrict: 'A',
